d9b099889f92bd825f0088ff84b980de5a6f9165
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-07-28 21:53:52 +1200

    FCC basic JS exercises - my solutions.

---
+++ scripts/variables.js
@@ -0,0 +1,140 @@
+// Free Code Camp solutions by saramdw
+
+/* A multi line comment 
+   can't be read within the comment lines */
+
+// First variable exercise
+
+    var myname;
+
+// Second variable exercise
+
+    var a;
+
+    a = 7;
+
+// Third variable exercise
+
+    var b;  // Works with let as well
+    b = a;
+
+    console.log(a)
+
+    // Initialising variables with assignment operator
+
+    var a = 9;
+
+    // Uninitialised variables
+
+    var a = 5;
+    var b = 10;
+    var c = "I am a";
+
+    // Understanding Case Sensitivity in variables
+
+    var StudlyCapVar;
+    var properCamelCase;
+    var TitleCaseOverride;
+    
+    studlyCapVar = 10;
+    properCamelCase = "A string";
+    titleCaseOver = 9000;
+
+    console.log(studlyCapVar)
+    console.log(properCamelCase)
+    console.log(titleCaseOver)
+
+    // Add two numbers with JavaScript
+
+    var sum = 10 + 0;
+
+    sum = 10 + 10;
+
+    console.log(sum)
+
+    // Subtracting with JS
+
+    var difference = 45 - 0;
+
+    difference - 33;
+
+    console.log(difference)
+
+    var ten = 20 - 10;
+
+    var quotient = 66 / 33;
+
+    // Increment with JS
+
+    var myVar = 87;
+
+    myVar++;
+
+    console.log(myVar)
+
+     // Decrement with JS
+
+        var myVar = 11
+
+        myVar--;
+        console.log(myVar)
+
+        // Decimal numbers Javascript
+        
+        var ourDecimal = 5.7;
+
+        var myDecimal = 5.5;
+
+        // Multiply decimals with JS 
+        
+        var product = 2.0 * 0.0;
+
+        product = 2.0 * 2.5;
+
+        console.log(product)
+
+        // Divide decimals JavaScript
+
+        var divide = 4.4 / 2.0;
+
+        console.log(divide)
+
+        // FInding the remainder 
+
+        var remainder;
+
+        remainder = 11 % 3;
+
+        console.log(remainder)
+
+        // Compnd Assignment with Augmented Addition
+        
+        var a = 3;
+        var b = 17;
+        var c = 10;
+
+        a += 12;
+        b += 9;
+        c += 7;
+
+        // Compund Assignment with Augmented Subtractions with
+
+        var a = 5;
+        var b = 4;
+        var c = 6;
+
+        a -= 3;
+        b -= 2;
+        c -= 4;
+
+        // 2021/07/28 session 19 FCC exercises in Basic Javascript completed
+
+
+
+
+
+
+
+
+
+
