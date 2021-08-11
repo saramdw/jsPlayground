1821ac3abed2fe014208548084616035569f94a3
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-08-11 20:57:56 +1200

    Completed functions.

--- scripts/functions.js
+++ scripts/functions.js
@@ -47,7 +47,66 @@ function myLocalScope() {
 
 myLocalScope();
 
-console.log('outside myLocalScope', myVar)
+// console.log('outside myLocalScope', myVar)
 
 // 2021/08/04 session FCC exercise 46 - 50th exercise complete.
 
+// Global vs local scope in functions
+
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+
+    var outerwear = "sweater";
+
+  return outerWear;
+}
+
+myOutfit();
+
+// Return a value from a function with return type
+
+function timesFive(num) {
+return num * 5
+}
+// var answer = timesfive(5)
+
+// Understand undefined value returned from a function
+
+var sum = 0;
+
+function addThree() {
+  sum = sum * 5;
+}
+
+function addFive() {
+    sum = sum + 5;
+}
+
+addThree();
+addFive();
+
+// Assignment with a returned value
+
+var processed = 0;
+
+function processArg(num) {
+return (num + 3) / 5;
+}
+
+processed = processArg(7)
+
+console.log(processed)
+
+// Stand in line
+
+function nextInLine(arr, item) {
+   
+    arr.push(item);
+    return arr.shift();
+    
+  }
+
+  console.log(nextInLine([1, 2, 3, 4, 5], 6));
+
+  // Completed 55 exercises of FCC
